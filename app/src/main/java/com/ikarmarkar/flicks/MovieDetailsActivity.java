6f427b63b53ec295f45cc8a4cd82227096aecf8b
Ishani Karmarkar <ikarmarkar@fb.com>
2018-06-29 20:06:48 -0700

    Fixed Mistakes

--- app/src/main/java/com/ikarmarkar/flicks/MovieDetailsActivity.java
+++ app/src/main/java/com/ikarmarkar/flicks/MovieDetailsActivity.java
@@ -1,17 +1,24 @@
 package com.ikarmarkar.flicks;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.LayerDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.widget.ImageView;
+import android.view.View;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.ikarmarkar.flicks.models.Movie;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.parceler.Parcels;
 
 public class MovieDetailsActivity extends AppCompatActivity {
@@ -24,12 +31,21 @@ public class MovieDetailsActivity extends AppCompatActivity {
     TextView tvOverview;
     RatingBar rbVoteAverage;
     RatingBar ratingBar;
-    ImageView ivPlay;
+
+    public final static String API_BASE_URl = "https://api_themoviedb.org/3";
+    public final static String API_KEY_PARAM = "api_key";
+    public final static String TAG ="MovieTrailerActivity";
+
+    String key;
+    String url;
+
+    AsyncHttpClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_details);
+        client = new AsyncHttpClient();
         // resolve the view objects
         tvTitle = (TextView) findViewById(R.id.tvTitle);
         tvOverview = (TextView) findViewById(R.id.tvOverview);
@@ -49,5 +65,37 @@ public class MovieDetailsActivity extends AppCompatActivity {
         ratingBar = (RatingBar) findViewById(R.id.rbVoteAverage);
         LayerDrawable stars = (LayerDrawable) ratingBar.getProgressDrawable();
         stars.getDrawable(2).setColorFilter(Color.RED, PorterDuff.Mode.SRC_ATOP);
+
+        getTrailer();
+    }
+
+    public void getTrailer() {
+        url = API_BASE_URl + "/movie/" + movie.getId() + "/videos";
+        RequestParams params = new RequestParams();
+        params.put(API_KEY_PARAM, getString(R.string.api_key));
+        client.get(url, params, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, cz.msebera.android.httpclient.Header[]headers, JSONObject response) {
+                try {
+                    JSONArray results = response.getJSONArray("results");
+                    for (int i = 0; i < results.length(); i++)
+                    {
+                        JSONObject jsonObject = results.getJSONObject(i);
+                        key = jsonObject.getString("key");
+                    }
+                    Log.i(TAG, String.format("Loaded %s movies", results.length()));
+                }
+                catch (JSONException e){
+                    Log.e(TAG, "Failed to parse trailer activity");
+                }
+            }
+        });
+    }
+
+    public void onTrailerClick(View v)
+    {
+        Intent intent = new Intent(this, MovieTrailerActivity.class);
+        intent.putExtra("Movie Key", key);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/ikarmarkar/flicks/MovieTrailerActivity.java
@@ -0,0 +1,33 @@
+package com.ikarmarkar.flicks;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class MovieTrailerActivity extends YouTubeBaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_trailer);
+
+        final String vId = getIntent().getStringExtra("Movie Kay");
+        YouTubePlayerView view = (YouTubePlayerView) findViewById(R.id.player);
+
+        view.initialize(getString(R.string.youtube_api_key), new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                youTubePlayer.cueVideo(vId);
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+                Log.e("MovieTrailerActivity", "Initialization Error");
+            }
+        });
+    }
+}
